8de023469c9ff5ab26c68712b3589f186c0badb6
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-04-06 23:50:04 -0300

    Encapsula processo de simulação do jogo

---
+++ lib/sims.py
@@ -0,0 +1,23 @@
+from progress.bar import ChargingBar
+from progress.spinner import PixelSpinner
+from .tictactoe import Game
+
+def run(player1, player2, accumulate=lambda: None, n=1000):
+    current_player = player1
+    info = None
+
+    spinner = PixelSpinner('Simulating games... ')
+    for _ in ChargingBar('Simulating games...   ', suffix="%(percent)d%% | %(elapsed)ds elapsed").iter(range(n)):
+        game = Game()
+        spinner.start()
+
+        while not game.isEnd():
+            move = current_player.getAction(game)
+            game.move(move)
+            current_player = player2 if current_player == player1 else player1
+            spinner.next()
+
+        info = accumulate(game, info)
+
+    spinner.finish()
+    return info
