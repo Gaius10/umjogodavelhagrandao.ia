257edea2a21a23096dfc2718393605e6127d8dad
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-04-06 23:44:24 -0300

    Arquivo para testes gerais

---
+++ dummy.py
@@ -0,0 +1,18 @@
+from lib.agents import RandomAgent, AlphaBetaAgent
+from lib.sims import run
+
+player1 = RandomAgent()
+player2 = RandomAgent()
+
+def accumulate(game, previous_info):
+    if previous_info is not None:
+        return previous_info + 1
+
+    return 1
+
+n = 10**4
+
+info = run(player1, player2, accumulate, n)
+
+print(f"Total games played: {n}")
+print(f"Total accumulated: {info}")
